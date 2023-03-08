12a1cbf30f6388d1772f26a7ce43eb46416053a5
Ignacio Bielsa <bielsa.arign22@zaragoza.salesianos.edu>
2023-03-08 13:28:09 +0100

    ANTES DE REALIZAR NADA

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		int[] arr = {2, 4, 1, 2, 1, 2, 4, 5};
+	     int[] arr = {2, 4, 1, 2, 1, 2, 4, 5};
 
 		int[] distinct = removeDuplicates(arr);
 		System.out.println(Arrays.toString(distinct));
